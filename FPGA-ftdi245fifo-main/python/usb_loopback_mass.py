2fa6d50f6c636a124e73649c2a51e02e58f5c703
Andy <drandyhaas@gmail.com>
2023-11-17 14:18:19 -0500

    pinouts to haasoscope v8.01

--- FPGA-ftdi245fifo-main/python/usb_loopback_mass.py
+++ FPGA-ftdi245fifo-main/python/usb_loopback_mass.py
@@ -31,7 +31,7 @@ TEST_COUNT = 2000
 if __name__ == '__main__':
     
     usb = USB_FTX232H_FT60X_sync245mode( device_to_open_list =
-        (('FTX232H', 'USB <-> Serial Converter'   ),           # firstly try to open FTX232H (FT232H or FT2232H) device named 'USB <-> Serial Converter'. Note that 'USB <-> Serial Converter' is the default name of FT232H or FT2232H chip unless the user has modified it. If the chip's name has been modified, you can use FT_Prog software to look up it.
+        (('FTX232H', 'Haasoscope USB2'   ),           # firstly try to open FTX232H (FT232H or FT2232H) device named 'USB <-> Serial Converter'. Note that 'USB <-> Serial Converter' is the default name of FT232H or FT2232H chip unless the user has modified it. If the chip's name has been modified, you can use FT_Prog software to look up it.
          ('FT60X'  , 'FTDI SuperSpeed-FIFO Bridge'))           # secondly try to open FT60X (FT600 or FT601) device named 'FTDI SuperSpeed-FIFO Bridge'. Note that 'FTDI SuperSpeed-FIFO Bridge' is the default name of FT600 or FT601 chip unless the user has modified it.
     )
     
